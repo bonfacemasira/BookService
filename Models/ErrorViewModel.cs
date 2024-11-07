67b89cd9a43e009a60da6f9af0d50109ad426500
bonfacemasira <bonfacemasira@gmail.com>
2024-11-07 10:27:00 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
